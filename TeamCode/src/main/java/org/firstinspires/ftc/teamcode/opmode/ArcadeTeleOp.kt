d5e68ee7a73e4040c4725dea4f4ba0b58d240b8e
Vincent Chen <vchen21@us.edu>
2019-02-01 10:22:46 -0500

    Rewrite teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/ArcadeTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/ArcadeTeleOp.kt
@@ -6,55 +6,89 @@ import org.firstinspires.ftc.teamcode.RobotHardware
 
 @TeleOp()
 class ArcadeTeleOp : LinearOpMode() {
-    val robot: RobotHardware by lazy { RobotHardware(this) }
+    private val robot: RobotHardware by lazy { RobotHardware(this) }
 
     override fun runOpMode() {
-        var rbPressedLast = false
-        var intakeDownBtnLast = false
-        var intakeDown = false;
-        var direction = 1
-        var powerScale = 1.0
+        // Some state for toggles
+        var g1ADisable = false
+        var slowMode = false
+        var g1BDisable = false
+        var reverseMode = false
+        var g2ADisable = false
+        var intakeDown = false
+
+        // Wait for the start button
         robot.reset()
         waitForStart()
 
+        // The main loop
         while (opModeIsActive()) {
-            // Invert the drive direction if necessary
-            if (gamepad1.right_bumper && !rbPressedLast) direction *= -1
-            rbPressedLast = gamepad1.right_bumper
 
-            val drive = (-gamepad1.right_stick_y * direction).toDouble()
+            // Update the state
+
+            // G1: a (toggle) => slow mode
+            if (gamepad1.a && !g1ADisable) slowMode = !slowMode
+            g1ADisable = gamepad1.a
+
+            // G1: b (toggle) => reverse
+            if (gamepad1.b && !g1BDisable) reverseMode = !reverseMode
+            g1BDisable = gamepad1.b
+
+
+            // G2: a (toggle) => Intake
+            if (gamepad2.a && !g2ADisable) intakeDown = !intakeDown
+            g2ADisable = gamepad2.a
+
+            // Move the robot
+
+            // G1: Right stick => arcade drive
+            val direction = if (reverseMode) -1.0 else 1.0
+            val powerScale = if (slowMode) 0.5 else 1.0
+            val drive = -gamepad1.right_stick_y * direction
             val turn = gamepad1.right_stick_x.toDouble()
-
             robot.leftPower = (drive + turn) * powerScale
             robot.rightPower = (drive - turn) * powerScale
-            robot.liftPower = -gamepad1.left_stick_y.toDouble()
 
-            robot.slidesPower = -gamepad1.left_stick_x.toDouble()
+            // G1: Left trigger => lift down; Right trigger => lift up
+            robot.liftPower = (gamepad1.right_trigger - gamepad1.left_trigger).toDouble()
 
-            robot.intakePower = gamepad2.left_trigger * (if (gamepad2.left_bumper) 1 else -1).toDouble()
+            // G1: Left Stick Y => Horiz slides in/out
+            robot.slidesPower = -gamepad1.left_stick_y.toDouble()
 
-            robot.intakePaddle = gamepad2.y
+            // G1: DPad Up => Unlock lift
+            if (gamepad1.dpad_up) robot.unlock()
 
-            if (gamepad2.b && ! intakeDownBtnLast)
-                intakeDown = !intakeDown
+            // G1: Left bumper => Reverse intake; Right bumper => Forward intake
+            // G2: Left trigger => Reverse intake; Right trigger => Forward intake
+            robot.intakePower = (gamepad2.right_trigger - gamepad2.left_trigger).toDouble() +
+                    (if (gamepad1.right_bumper) 1.0 else if (gamepad1.left_bumper) -1.0 else 0.0)
+
+            // G2: Left bumper => Depositor Up; Right bumper = Depositor Down
+            if (gamepad2.left_bumper)
+                robot.deposit = false
+            else if (gamepad2.right_bumper)
+                robot.deposit = true
+
+            // G2: B => Paddle
+            robot.intakePaddle = true
+            //robot.intakePaddle = gamepad2.b
+
+            // Deal with intakeDown toggle
             robot.intakeDown = intakeDown
-            intakeDownBtnLast = gamepad2.b
 
-            robot.deposit = gamepad2.a
-            if (gamepad2.x) robot.unlock()
+            // Telemetry and debug
 
-            powerScale = if (gamepad1.left_bumper) 0.5 else 1.0
+            // G1: DPad Down => Reset Encoders (For testing)
+            if (gamepad1.dpad_down) robot.resetEncoders()
 
-            if (gamepad1.b)
-                robot.resetEncoders()
-
-            //telemetry.addData("Right pos", robot.rightPosition)
-            //telemetry.addData("Left pos", robot.leftPosition)
+            // Telemetry
+            telemetry.addData("Right Pos", robot.rightPosition)
+            telemetry.addData("Left Pos", robot.leftPosition)
+            telemetry.addLine("-----")
             telemetry.addData("Right Front Pos", robot.frontRightPosition)
             telemetry.addData("Right Back Pos", robot.backRightPosition)
             telemetry.addData("Left Front Pos", robot.frontLeftPosition)
             telemetry.addData("Left Back Pos", robot.backLeftPosition)
-
             telemetry.update()
         }
 
